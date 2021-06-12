817f416dd73c4fbe97508f2aa1fbfb83cf613f02
Abhinav Ramachandran <geckods@gmail.com>
2021-06-13 00:36:58 +0530

    There was an overflow bug with maxInt, fixed now.

--- util.go
+++ util.go
@@ -8,7 +8,7 @@ const UintSize = 32 << (^uint(0) >> 32 & 1) // 32 or 64
 
 
 const (
-	MaxInt  = 1<<(UintSize-1) - 1 // 1<<31 - 1 or 1<<63 - 1
+	MaxInt  = 1<<(UintSize-2) - 1 // 1<<31 - 1 or 1<<63 - 1
 	MaxUint = 1<<UintSize - 1     // 1<<32 - 1 or 1<<64 - 1
 )
 
